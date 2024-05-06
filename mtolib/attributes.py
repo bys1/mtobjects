7915cd10f6ae27c32d45f1a07f96e40105ddef08
Björn Schönrock <bjorn.schonrock@live.nl>
2024-05-06 14:59:07 +0200

    stuff

--- mtolib/attributes.py
+++ mtolib/attributes.py
@@ -2,10 +2,11 @@ from mtolib.maxtree import MaxTree
 
 import numpy as np
 
-from math import sqrt, exp, pi
+from math import sqrt, exp, log, pi
 from matplotlib import pyplot as plt
 
 from scipy.special import gammaincinv, gammainc
+from scipy.optimize import curve_fit
 
 MT_HAVE_SIGNIFICANT_DESCENDANT = 4
 MT_HAVE_DESCENDANT = 16
@@ -55,20 +56,51 @@ def get_attributes(mto_struct, tree : MaxTree, object_id):
         ysi.append(I)
 
         node_id = one_up(mto_struct, node_id)
+    
+    I += V_prev / area
+    I_e = I / 2
+    for R, I_cur in zip(xs, ysi):
+        if I_cur > I_e:
+            break
+        R_e = R
 
     n = 4
     print('I', I, 'V_e', V_e, 'I_e', I_e)
     while object_id != -1:
-        L = sersic_luminosity(tree, object_id, n, R_e, I_e)
-        I = sersic_intensity (tree, object_id, n, R_e, I_e)
+        R = sqrt(tree.nodes[object_id].area * PI_DIV)
+        L = sersic_luminosity(tree, object_id, n, R_e, I_e) * 4000
+        I = sersic_intensity (n, R, R_e, I_e)
         zs.append(L)
         zsi.append(I)
         object_id = one_up(mto_struct, object_id)
     
-    xs = [R / R_e for R in xs]
+    xs0 = [R / R_e for R in xs]
 
-    plt.plot(xs, ysi)
-    plt.plot(xs, zsi)
+    plt.plot(xs0, ysi)
+    plt.plot(xs0, zsi)
+    plt.ylim(ymin=0, ymax=12000)
+    plt.show()
+
+    formula = lambda xs, n: [sersic_intensity(n, x, R_e, I_e) for x in xs]
+
+    """
+    cnt = sum([x > 1 for x in xs0])
+    plt.plot(xs0[:cnt], ysi[:cnt])
+    plt.plot(xs0[:cnt], zsi[:cnt])
+    plt.show()
+    """
+
+    plt.plot(xs0, [log(y) for y in ysi])
+    plt.plot(xs0, [log(z) for z in zsi])
+    plt.show()
+
+    popt, _ = curve_fit(formula, xs, ysi, bounds=(0,12))
+    n_fit = popt[0]
+    print('n_fit =', n_fit)
+    sersic_fit = formula(xs, n_fit)
+
+    plt.plot(xs0, ysi)
+    plt.plot(xs0, sersic_fit)
     plt.ylim(ymin=0, ymax=12000)
     plt.show()
 
@@ -79,9 +111,8 @@ def sersic_luminosity(tree : MaxTree, node_id, n, R_e, I_e):
     L = I_e * R_e * R_e * pi * 2 * n * exp(b_n) / (b_n ** (2*n)) * gammainc(2*n, x)
     return L
 
-def sersic_intensity(tree : MaxTree, node_id, n, R_e, I_e):
+def sersic_intensity(n, R, R_e, I_e):
     b_n = gammaincinv(2*n, 0.5)
-    R = sqrt(tree.nodes[node_id].area * PI_DIV)
     ex = -b_n * ((R/R_e) ** (1/n) - 1)
     return I_e * exp(ex)
 
